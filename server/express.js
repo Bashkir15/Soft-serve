dda664a04f0a97d3f7a7711a6fd72feb9446f5ae
Forrest Collins <Forest.d.collins@gmail.com>
2016-12-29 21:13:51 -0600

    Added the express file and routes file on the server for the docs site

---
+++ server/express.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import ejs from 'ejs'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
+
+import indexRoutes from './routes'
+
+module.exports = () => {
+	let app = express();
+
+	app.set('view engine', 'ejs');
+	app.set('views', path.join(__dirname, '../docs/views'));
+
+	app.use(morgan('dev'));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(express.static(path.join(__dirname, '../docs')));
+	app.use(express.static(path.join(__dirname, '../dist')));
+	app.use(express.static(path.join(__dirname, '../node_modules')));
+
+	app.use('/', indexRoutes);
+
+	return app;
+}
