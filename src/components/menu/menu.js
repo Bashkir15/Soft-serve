8466946a12ddc224d46de4e3657f3adbf77971f5
Forrest Collins <Forest.d.collins@gmail.com>
2017-01-09 13:42:29 -0600

    Refactoring the menu code, making it cleaner, caching dom elements for performance

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,6 +4,7 @@ class menu {
 		this.container = null;
 		this.outline = null;
 		this.menuTrigger = null;
+		this.items = null;
 
 		this.defaults = {
 			element: '.soft-menu',
@@ -41,6 +42,94 @@ class menu {
 		this._init();
 	}
 
+
+	/**
+	 *
+	 	Public Methods
+	 *
+	**/
+
+
+
+	_show(e) {
+		if (this.element && this.container) {
+			var height = this.element.getBoundingClientRect().height;
+			var width = this.element.getBoundingClientRect().width;
+
+			this.container.style.width = width + 'px';
+			this.container.style.height = height + 'px';
+			this.outline.style.width = width + 'px';
+			this.outline.style.height = height + 'px';
+
+			var transitionDuration = this.defaults.transitionDuration * this.defaults.transitionFraction;
+
+			// Calculate transition delays for each menu item so they fade in order
+			var items = this.element.querySelectorAll('.' + this.classes.item);
+			for (let i = 0; i < items.length; i++) {
+				var itemDelay = null;
+
+				if (this.element.classList.contains(this.classes.topLeft) || this.element.classList.contains(this.classes.topRight)) {
+					itemDelay = ((height - items[i].offsetTop - items[i].offsetHeight) / height * transitionDuration) + 's';
+				} else {
+					itemDelay = (items[i].offsetTop / height * transitionDuration) + 's';
+				}
+
+				items[i].style.transitionDelay = itemDelay;
+			}
+
+
+
+			this._applyClip(height, width);
+
+			window.requestAnimationFrame(() => {
+				this.element.classList.add(this.classes.animating);
+				this.element.style.clip = 'rect(0 ' + width + 'px ' + height + 'px 0)';
+				this.container.classList.add(this.classes.visible);  
+			});
+
+			this._animationEndListener();
+
+			var callback = function(evt) {
+				if (evt !== e && evt.target.parentNode !== this.element) {
+					//document.removeEventListener('click', callback);
+					this.hide();
+				} 
+			}.bind(this);
+
+			document.addEventListener('click', callback);
+		}
+	}
+
+	_hide() {
+		if (this.element && this.container) {
+			var items = this.element.querySelectorAll('.' + this.classes.item);
+
+			for (let i = 0; i < items.length; i++) {
+				items[i].style.removeProperty('transition-delay');
+			}
+
+			// measure the inner element
+
+			var rect = this.element.getBoundingClientRect();
+			var height = rect.height;
+			var width = rect.width;
+
+			this.element.classList.add(this.classes.animating);
+			this._applyClip(height, width);
+			this.container.classList.remove(this.classes.visible);
+		}
+	}
+
+	_toggle(e) {
+		if (this.container.classList.contains(this.classes.visible)) {
+			this.hide();
+		} else {
+			this.show(e)
+		}
+	}
+
+
+
 	_applySettings(options) {
 		if (typeof options === 'object') {
 			for (var i in options) {
@@ -52,22 +141,35 @@ class menu {
 	}
 
 	_init() {
+
+		// Declare the component variables for the menu -- Menu container,
+		// Menu outline, Menu trigger button, Menu trigger Id, menu items
+		var container = document.createElement('div');
+		var outline = document.createElement('div');
+		var menuTrigger = null;
+		var menuId = null;
+		var items = null;
+
+		// Retrieve elements from DOM based on user-provided class
+
 		this.element = document.querySelector(this.defaults.element);
 
-		var container = document.createElement('div');
-		container.classList.add(this.classes.container);
+		// Restructure the DOM to append the container and outline
+
 		this.element.parentNode.insertBefore(container, this.element);
 		this.element.parentNode.removeChild(this.element);
+		container.classList.add(this.classes.container);
 		container.appendChild(this.element);
-		this.container = container;
 
-		var outline = document.createElement('div');
 		outline.classList.add(this.classes.outline);
+
+		this.container = container;
 		this.outline = outline;
 		this.container.insertBefore(outline, this.element);
 
-		var menuId = this.element.getAttribute('for');
-		var menuTrigger = null;
+		// Retrieve the menuId and then find the Trigger matching that Id
+
+		menuId = this.element.getAttribute('for');
 
 		if (menuId) {
 			menuTrigger = document.getElementById(menuId);
@@ -78,11 +180,21 @@ class menu {
 			}
 		}
 
-		var items = this.element.querySelectorAll('.' + this.classes.item);
-		var itemKeyHandler = this._itemKeyHandler.bind(this);
-		var itemClickHandler = this._itemClickHandler.bind(this);
+		// Retrieve Items from the DOM and store them in the constructor
+		// so the DOM doesn't have to be queried multiple times and then
+		// attach events to the items
 
-		for (let i = 0; i < items.length; i++) {
+		items = this.element.querySelectorAll('.' + this.classes.item);
+		this.items = items;
+
+		this._attachItemEvents();
+
+		// Handle the positioning of the outline so it will be inline with the
+		// Menu
+
+		this._positionOutline();
+
+		/* for (let i = 0; i < items.length; i++) {
 			items[i].addEventListener('click', itemClickHandler);
 			items[i].tabIndex = '-1';
 			items[i].addEventListener('keydown', itemKeyHandler);
@@ -102,7 +214,7 @@ class menu {
 
 		if (this.element.classList.contains(this.classes.topRight)) {
 			this.outline.classList.add(this.classes.topRight);
-		}
+		} */
 
 	}
 
@@ -202,83 +314,7 @@ class menu {
 		}
 	}
 
-	_show(e) {
-		if (this.element && this.container) {
-			var height = this.element.getBoundingClientRect().height;
-			var width = this.element.getBoundingClientRect().width;
-
-			this.container.style.width = width + 'px';
-			this.container.style.height = height + 'px';
-			this.outline.style.width = width + 'px';
-			this.outline.style.height = height + 'px';
-
-			var transitionDuration = this.defaults.transitionDuration * this.defaults.transitionFraction;
-
-			// Calculate transition delays for each menu item so they fade in order
-			var items = this.element.querySelectorAll('.' + this.classes.item);
-			for (let i = 0; i < items.length; i++) {
-				var itemDelay = null;
-
-				if (this.element.classList.contains(this.classes.topLeft) || this.element.classList.contains(this.classes.topRight)) {
-					itemDelay = ((height - items[i].offsetTop - items[i].offsetHeight) / height * transitionDuration) + 's';
-				} else {
-					itemDelay = (items[i].offsetTop / height * transitionDuration) + 's';
-				}
-
-				items[i].style.transitionDelay = itemDelay;
-			}
-
-
-
-			this._applyClip(height, width);
-
-			window.requestAnimationFrame(() => {
-				this.element.classList.add(this.classes.animating);
-				this.element.style.clip = 'rect(0 ' + width + 'px ' + height + 'px 0)';
-				this.container.classList.add(this.classes.visible);  
-			});
-
-			this._animationEndListener();
-
-			var callback = function(evt) {
-				console.log('arrrrrrrrrrrrrr');
-				if (evt !== e && evt.target.parentNode !== this.element) {
-					//document.removeEventListener('click', callback);
-					this.hide();
-				} 
-			}.bind(this);
-
-			document.addEventListener('click', callback);
-		}
-	}
-
-	_hide() {
-		if (this.element && this.container) {
-			var items = this.element.querySelectorAll('.' + this.classes.item);
-
-			for (let i = 0; i < items.length; i++) {
-				items[i].style.removeProperty('transition-delay');
-			}
-
-			// measure the inner element
-
-			var rect = this.element.getBoundingClientRect();
-			var height = rect.height;
-			var width = rect.width;
-
-			this.element.classList.add(this.classes.animating);
-			this._applyClip(height, width);
-			this.container.classList.remove(this.classes.visible);
-		}
-	}
-
-	_toggle(e) {
-		if (this.container.classList.contains(this.classes.visible)) {
-			this.hide();
-		} else {
-			this.show(e)
-		}
-	}
+	
 
 	_animationEndListener() {
 		let removeAnimationEndListener = this._removeAnimationEndListener.bind(this);
