c881b8672fedfd61865a0f8ee4ed9088441823ff
Forrest Collins <Forest.d.collins@gmail.com>
2017-01-08 19:05:36 -0600

    Added some keycodes to the constructor for the menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -11,7 +11,15 @@ class menu {
 			transitionFraction: 0.8,
 			closeTimeout: 150
 		};
-		this.keycodes = {};
+		
+		this.keycodes = {
+			enter: 13,
+			escape: 27,
+			space: 32,
+			up: 38,
+			down: 40
+		};
+
 		this.classes = {
 			container: 'soft-menu-container',
 			outline: 'soft-menu-outline',
