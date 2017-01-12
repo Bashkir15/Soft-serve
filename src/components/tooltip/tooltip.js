83ac65dbffe7c6028e4286aa7cdd193e17cb09eb
Forrest Collins <Forest.d.collins@gmail.com>
2017-01-11 23:54:22 -0600

    Added the hide method for the tooltips

--- src/components/tooltip/tooltip.js
+++ src/components/tooltip/tooltip.js
@@ -1,6 +1,11 @@
 class tooltip {
 	constructor(options) {
 		this.element = null;
+		this.parent = null;
+
+		this.defaults = {
+			element: '.soft-tooltip'
+		};
 
 		this.classes = {
 			active: 'soft-tooltip-active',
@@ -9,5 +14,23 @@ class tooltip {
 			right: 'soft-tooltip-right',
 			top: 'soft-tooltip-right'
 		};
+	}
+
+	_hide() {
+		this.element.classList.remove(this.classes.active);
+	}
+
+	_init() {
+		this.element = document.querySelector(this.defaults.element);
+
+		if (this.element) {
+			let tooltipParent = this.element.getAttribute('for');
+
+			if (tooltipParent) {
+				this.parent = document.getElementById(tooltipParent);
+
+				this._attachEvents();
+			}
+		}
 	}
 }
