c813cfdd88a41da5281160ee16f93ea292cb6f45
Forrest Collins <Forest.d.collins@gmail.com>
2017-01-11 22:22:49 -0600

    Added test for animating on toggle

--- tests/unit/menu.js
+++ tests/unit/menu.js
@@ -40,5 +40,18 @@ describe('SoftserveMenu', function() {
 				done();
 			}, 100);
 		});
+
+		it('should start animating on toggle()', function(done) {
+			expect($(element.parentElement)).to.not.have.class('soft-menu-visible');
+			menu.toggle();
+			window.setTimeout(function() {
+				expect($(element.parentElement)).to.have.class('soft-menu-visible');
+
+				var event = document.createEvent('HTMLEvents');
+				event.initEvent('transitionend', true, true);
+				element.dispatchEvent(event);
+				done();
+			}, 100);
+		});
 	});
 });
