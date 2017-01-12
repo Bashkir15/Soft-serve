2b55c6e6e07d74630b80462ff47ab9c3cf3d6050
Forrest Collins <Forest.d.collins@gmail.com>
2017-01-12 00:03:03 -0600

    Added the handler for the mouseEnter event

--- src/components/tooltip/tooltip.js
+++ src/components/tooltip/tooltip.js
@@ -33,4 +33,44 @@ class tooltip {
 			}
 		}
 	}
+
+	_handleMouseEvent(e) {
+		let rect = e.target.getBoundingClientRect();
+		let left = rect.left + (ret.width / 2);
+		let top = rect.top + (rect.height / 2);
+		let marginLeft = -1 * (this.element.offsetWidth / 2);
+		let marginTop = -1 * (this.element.offsetHeight / 2);
+
+		if (this.element.classList.contains(this.classes.left) || this.element.classList.contains(this.classes.right)) {
+			left = (rect.width / 2);
+
+			if (top + marginTop < 0) {
+				this.element.style.top = '0';
+				this.element.style.marginTop = '0';
+			} else {
+				this.element.style.top = top + 'px';
+				this.element.style.marginTop = marginTop + 'px';
+			}
+		} else {
+			if (left + marginLeft < 0) {
+				this.element.style.left = '0';
+				this.element.style.marginLeft = '0';
+			} else {
+				this.element.style.left = left + 'px';
+				this.element.style.marginLeft = marginLeft + 'px';
+			}
+		}
+
+		if (this.element.classList.contains(this.classes.top)) {
+			this.element.style.top = rect.top - this.element.offsetHeight - 10 + 'px';
+		} else if (this.element.classList.contains(this.classes.right)) {
+			this.element.style.left = rect.left + rect.width + 10 + 'px';
+		} else if (this.element.classList.contains(this.classes.left)) {
+			this.element.style.left = rect.left - this.element.offsetWidth - 10 + 'px';
+		} else {
+			this.element.style.top = rect.top + rect.height + 10 + 'px';
+		}
+
+		this.element.classList.add(this.classes.active);
+	}
 }
