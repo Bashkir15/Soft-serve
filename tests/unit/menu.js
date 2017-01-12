f5e8254d5b85e177b83ba0e5873f94b842903de5
Forrest Collins <Forest.d.collins@gmail.com>
2017-01-11 22:17:13 -0600

    Started writing the tests for the menu component

---
+++ tests/unit/menu.js
@@ -0,0 +1,31 @@
+import * as softServe from '../../dist/soft-serve'
+
+describe('SoftserveMenu', function() {
+	describe('visibility function', function() {
+		var parent;
+		var element;
+
+		before(function() {
+			parent = document.createElement('div');
+			element = document.createElement('ul');
+			element.classList.add('.test-element');
+			parent.appendChild(element);
+			var menu = new softServe.menu({
+				element: '.test-element'
+			});
+		});
+
+		it('should start animating on show()', function(done) {
+			expect($(element.parentElement)).to.not.have.class('soft-menu-visible');
+			menu.show();
+			window.setTimeout(function() {
+				expect($(element.parentElement)).to.have.class('soft-menu-visible');
+
+				var event = document.createEvent('HTMLEvents');
+				event.initEvent('transitionend', true, true);
+				element.dispatchEvent(event);
+				done();
+			}, 100);
+		});
+	});
+});
